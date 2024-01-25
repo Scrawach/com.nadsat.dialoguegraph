9a78e973cb47b7734c74b9132e51c38c930ea535
scrawach <scrawach@gmail.com>
2024-01-25 09:46:41 +0300

    added buttons node view

--- NodeGraphExperiment/Assets/Editor/Drawing/Nodes/ButtonsNodeView.cs
+++ NodeGraphExperiment/Assets/Editor/Drawing/Nodes/ButtonsNodeView.cs
@@ -4,6 +4,8 @@ namespace Editor.Drawing.Nodes
 {
     public class ButtonsNodeView : Node
     {
-        
+        private const string UxmlPath = "Assets/Editor/Resources/UXML/ButtonsNodeView.uxml";
+
+        public ButtonsNodeView() : base(UxmlPath) { }
     }
 }
