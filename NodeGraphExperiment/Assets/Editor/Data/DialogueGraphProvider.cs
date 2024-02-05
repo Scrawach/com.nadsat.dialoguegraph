070e915ae96070dfe5047a53a0c8f84900f89e75
scrawach <scrawach@gmail.com>
2024-02-05 12:30:45 +0300

    introduced dialogue graph provider that storage folder and asset path

---
+++ NodeGraphExperiment/Assets/Editor/Data/DialogueGraphProvider.cs
@@ -0,0 +1,19 @@
+﻿using Runtime;
+
+namespace Editor.Data
+{
+    public class DialogueGraphProvider
+    {
+        public DialogueGraphContainer Container;
+
+        public string GetFolderPath()
+        {
+            return string.Empty;
+        }
+
+        public string GetAssetPath()
+        {
+            return string.Empty;
+        }
+    }
+}
