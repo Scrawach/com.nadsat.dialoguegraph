d87187bd435179e22bae5c0f71885657cbd60202
scrawach <scrawach@gmail.com>
2024-02-09 14:15:26 +0300

    implemented pasting copied nodes around mouse position

--- NodeGraphExperiment/Assets/Editor/Manipulators/CopyPasteManipulator.cs
+++ NodeGraphExperiment/Assets/Editor/Manipulators/CopyPasteManipulator.cs
@@ -1,16 +1,23 @@
 using System.Collections.Generic;
+using System.Linq;
+using Editor.Drawing.Nodes;
 using Editor.Serialization;
 using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Editor.Manipulators
 {
     public class CopyPasteManipulator : Manipulator
     {
+        private const string PasteOperationName = "Paste";
+        private const string DuplicateOperationName = "Duplicate";
+        
         private readonly CopyPaste _copyPaste;
         private readonly CopyPasteFactory _copyPasteFactory;
 
         private GraphView _graphView;
+        private Vector2 _mousePosition;
 
         public CopyPasteManipulator(CopyPaste copyPaste, CopyPasteFactory factory)
         {
@@ -23,13 +30,18 @@ namespace Editor.Manipulators
             _graphView = (GraphView) target;
             _graphView.serializeGraphElements += OnCutCopyOperation;
             _graphView.unserializeAndPaste += OnPasteOperation;
+            _graphView.RegisterCallback<MouseMoveEvent>(OnMouseMoved);
         }
 
         protected override void UnregisterCallbacksFromTarget()
         {
             _graphView.serializeGraphElements -= OnCutCopyOperation;
             _graphView.unserializeAndPaste -= OnPasteOperation;
+            _graphView.UnregisterCallback<MouseMoveEvent>(OnMouseMoved);
         }
+
+        private void OnMouseMoved(MouseMoveEvent evt) =>
+            _mousePosition = evt.mousePosition;
 
         private string OnCutCopyOperation(IEnumerable<GraphElement> elements) =>
             _copyPaste.ToJson(elements);
@@ -37,9 +49,34 @@ namespace Editor.Manipulators
         private void OnPasteOperation(string operationName, string data)
         {
             _graphView.ClearSelection();
-            var copiedData = _copyPaste.FromJson(data);
-            foreach (var graphElement in _copyPasteFactory.Create(copiedData)) 
+            var elements = CopiedElementsFromJson(data);
+            
+            if (operationName == PasteOperationName) 
+                UpdatePositionForCopiedElements(elements);
+
+            foreach (var graphElement in elements) 
                 _graphView.AddToSelection(graphElement);
+        }
+
+        private void UpdatePositionForCopiedElements(IEnumerable<GraphElement> elements)
+        {
+            var modelHandlers = elements.OfType<IModelHandle>().ToArray();
+            var firstModel = modelHandlers.First();
+            var localMousePosition = _graphView.contentViewContainer.WorldToLocal(_mousePosition);
+            var difference = firstModel.Model.Position - localMousePosition;
+            
+            foreach (var handle in modelHandlers)
+            {
+                var previousPosition = handle.Model.Position;
+                var newPosition = previousPosition - difference;
+                handle.Model.SetPosition(new Rect(newPosition, Vector2.zero));
+            }
+        }
+
+        private GraphElement[] CopiedElementsFromJson(string json)
+        {
+            var copiedData = _copyPaste.FromJson(json);
+            return _copyPasteFactory.Create(copiedData).ToArray();
         }
     }
 }
