d05b0af9a9af5b47ee1f6d7decdcc50ecf4b95ce
scrawach <scrawach@gmail.com>
2024-02-18 00:01:55 +0300

    added case for packages editor resources

--- NodeGraphExperiment/Assets/Editor/Paths/EditorPath.cs
+++ NodeGraphExperiment/Assets/Editor/Paths/EditorPath.cs
@@ -1,14 +1,23 @@
 ﻿using System.IO;
+using UnityEditor;
 
 namespace Editor.Paths
 {
     public static class EditorPath
     {
+        private const string DefaultResourcesPath = "Assets/Editor/Resources";
+        private const string PackagesResourcesPath = "Packages/com.scrawach.DialogueGraph/Editor/Resources";
+        
         public static string ToAbsolutePathUxml(string relativePath)
         {
-            var result = Path.Combine("Assets/Editor/Resources", relativePath);
-            var resultWithExtension = result + ".uxml";
-            return resultWithExtension;
+            var result = Path.Combine(DefaultResourcesPath, relativePath);
+            var resource = EditorGUIUtility.Load(result);
+
+            if (resource == null) 
+                result = Path.Combine(PackagesResourcesPath, relativePath);
+
+            var withExtension = result + ".uxml";
+            return withExtension;
         }
     }
 }
