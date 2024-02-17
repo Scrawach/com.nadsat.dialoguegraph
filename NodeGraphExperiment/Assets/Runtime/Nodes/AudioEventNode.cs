3582feadd5d0c979f3004cc80fafb8e41407bd67
scrawach <scrawach@gmail.com>
2024-02-17 17:59:14 +0300

    a little bit refactoring for views, added few events to one node

--- NodeGraphExperiment/Assets/Runtime/Nodes/AudioEventNode.cs
+++ NodeGraphExperiment/Assets/Runtime/Nodes/AudioEventNode.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Runtime.Nodes
 {
     [Serializable]
     public class AudioEventNode : BaseDialogueNode
     {
+        public List<AudioEventData> Events;
+    }
+
+    [Serializable]
+    public class AudioEventData
+    {
         public string EventName;
+        public float Delay;
     }
 }
