0a5a9361a2b9b35aeaca2f6ce80b3b8aa7e87b11
scrawach <scrawach@gmail.com>
2024-02-20 14:02:14 +0300

    some changes for future drag and drop manipulator

--- NodeGraphExperiment/Assets/Editor/Manipulators/DragAndDropInspectorManipulator.cs
+++ NodeGraphExperiment/Assets/Editor/Manipulators/DragAndDropInspectorManipulator.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -5,14 +6,17 @@ namespace Editor.Manipulators
 {
     public class DragAndDropInspectorManipulator : PointerManipulator
     {
+        private readonly VisualElement _root;
+
         private Vector2 _targetStartPosition;
         private Vector3 _pointerStartPosition;
         private bool _enabled;
-        private VisualElement _root;
-        
+
+        public DragAndDropInspectorManipulator(VisualElement container) => 
+            _root = container;
+
         protected override void RegisterCallbacksOnTarget()
         {
-            _root = target.parent;
             target.RegisterCallback<PointerDownEvent>(OnPointerDown);
             target.RegisterCallback<PointerMoveEvent>(OnPointerMove);
             target.RegisterCallback<PointerUpEvent>(OnPointerUp);
@@ -33,6 +37,9 @@ namespace Editor.Manipulators
             _pointerStartPosition = evt.position;
             target.CapturePointer(evt.pointerId);
             _enabled = true;
+            
+            _root.parent.Add(target);
+            _root.Remove(target);
         }
         
         private void OnPointerMove(PointerMoveEvent evt)
@@ -42,14 +49,49 @@ namespace Editor.Manipulators
             
             var delta = evt.position - _pointerStartPosition;
             target.transform.position = new Vector2(_targetStartPosition.x + delta.x, _targetStartPosition.y + delta.y);
+            ReconstructParentVisualTree();
         }
-        
+
+        private void ReconstructParentVisualTree()
+        {
+            var container = _root.Q<VisualElement>("button-container").Children().ToArray();
+            for (var i = 0; i < container.Length; i++)
+            {
+                if (container[i] == target)
+                    continue;
+
+                var item = container[i];
+                var itemPosition = item.worldTransform.GetPosition();
+                Debug.Log($"{target.worldTransform.GetPosition().y}, {itemPosition.y}");
+                
+            }
+
+            var targetPosition = target.worldTransform.GetPosition();
+            var offset = 0;
+            for (var i = 0; i < container.Length; i++)
+            {
+                if (container[i] == target)
+                    continue;
+
+                var item = container[i];
+                var itemPosition = item.worldTransform.GetPosition();
+
+                if (itemPosition.y > targetPosition.y)
+                {
+                    
+                }
+            }
+        }
+
         private void OnPointerUp(PointerUpEvent evt)
         {
             if (IsAnotherPointer(evt)) 
                 return;
             
             target.ReleasePointer(evt.pointerId);
+            _root.parent.Remove(target);
+            _root.Add(target);
+
         }
         
         private void OnPointerCaptureOut(PointerCaptureOutEvent evt)
