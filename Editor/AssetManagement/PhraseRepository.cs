18b5046b22b2091c7e44a083f7b9bc8d07bad293
scrawach <scrawach@gmail.com>
2024-04-03 16:38:27 +0300

    removed unused code on phrase repository

--- Editor/AssetManagement/PhraseRepository.cs
+++ Editor/AssetManagement/PhraseRepository.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Nadsat.DialogueGraph.Editor.Localization;
-using Nadsat.DialogueGraph.Runtime.Localization;
 
 namespace Nadsat.DialogueGraph.Editor.AssetManagement
 {
@@ -24,43 +20,5 @@ namespace Nadsat.DialogueGraph.Editor.AssetManagement
 
         public bool Remove(string phraseId) =>
             _table.Remove(phraseId);
-    }
-
-    public class PhraseRepositoryOld
-    {
-        private readonly Dictionary<string, LocalizedString> _content = new();
-
-        private LocalizedTable _localizedTable;
-
-        public string CurrentLanguage { get; private set; }
-
-        public event Action<string> LanguageChanged;
-
-        public void Initialize()
-        {
-            var table = new CsvAsset("phrases");
-            _localizedTable = new LocalizedTable(table);
-
-            foreach (var (key, localizedString) in _localizedTable.LoadLocalizedStrings())
-                _content[key] = localizedString;
-
-            var firstLanguageInTable = _localizedTable.GetAvailableLanguages().First();
-            ChangeLanguage(firstLanguageInTable);
-        }
-
-        public void ChangeLanguage(string target)
-        {
-            CurrentLanguage = target;
-            LanguageChanged?.Invoke(target);
-        }
-
-        public string[] AllLanguages() =>
-            _localizedTable.GetAvailableLanguages();
-
-        public string[] AllKeys() =>
-            _content.Keys.ToArray();
-
-        public string Get(string key) =>
-            _content[key][CurrentLanguage];
     }
 }
